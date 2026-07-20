8ad21f4fb8e14341b063b4154d97a598db562a9b
Sumit Garg <sumit.garg@oss.qualcomm.com>
2026-07-20 19:09:51 +0530

    feat(plat/qti/wildcat): add common platform base layer
    Add the minimal Wildcat architecture base shared by Wildcat platforms:
    the core-position helper (wildcat_helpers.S) and an empty BL31 setup
    post-hook override that per-driver patches extend. Common headers and
    the BL31 setup backbone are reused from plat/qti/common.
    
    Change-Id: I195b1df91bef4c886d69dd8f91f5703cc1647508
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

---
+++ plat/qti/wildcat/common/wildcat_bl31_setup.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026 Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <qti_plat.h>
+
+void plat_qti_bl31_setup_post(void)
+{
+}
