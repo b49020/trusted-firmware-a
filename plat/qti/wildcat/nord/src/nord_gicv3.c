07e5dbf42a75371015966d9430442a0eb55093bd
Sumit Garg <sumit.garg@oss.qualcomm.com>
2026-07-20 19:48:27 +0530

    feat(qti): add Nord SA8797P platform support on Wildcat
    Add the Nord (SA8797P) platform on the Wildcat architecture: platform
    definitions and memory map, translation-table setup, PSCI/GIC wiring and
    the BL31 setup skeleton. Bring up the Oryon-1 CPU library
    (lib/cpus/aarch64/oryon.S) with its reset-time setup and the QCCE-326
    erratum flag (gated to affected Oryon-1 revisions, disabled on Nord
    silicon), and route the BL31 console to the physical GENI UART. sec_core
    uses a stub backend that a follow-up per-driver patch replaces with the
    SoC configuration.
    
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Change-Id: Ie80de8b166a3e6eb5136d44e07af1de1fd7cf325

---
+++ plat/qti/wildcat/nord/src/nord_gicv3.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015-2024, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2018-2024, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <lib/utils_def.h>
+
+#include <drivers/arm/gicv3.h>
+
+#include <nord_ints.h>
+
+/* Array of interrupts to be configured by the gic driver */
+const interrupt_prop_t qti_interrupt_props[] = {
+	INTR_PROP_DESC(NORD_INT_ID_CPU_WAKEUP_SGI,
+		       GIC_HIGHEST_SEC_PRIORITY, INTR_GROUP0,
+		       GIC_INTR_CFG_EDGE),
+	INTR_PROP_DESC(NORD_INT_ID_RESET_SGI, GIC_HIGHEST_SEC_PRIORITY,
+		       INTR_GROUP0,
+		       GIC_INTR_CFG_EDGE),
+	INTR_PROP_DESC(NORD_INT_ID_SEC_WDOG_BARK, GIC_HIGHEST_SEC_PRIORITY,
+		       INTR_GROUP0,
+		       GIC_INTR_CFG_EDGE),
+	INTR_PROP_DESC(NORD_INT_ID_NON_SEC_WDOG_BITE,
+		       GIC_HIGHEST_SEC_PRIORITY, INTR_GROUP0,
+		       GIC_INTR_CFG_LEVEL),
+	INTR_PROP_DESC(NORD_INT_ID_VMIDMT_ERR_CLT_SEC,
+		       GIC_HIGHEST_SEC_PRIORITY, INTR_GROUP0,
+		       GIC_INTR_CFG_EDGE),
+	INTR_PROP_DESC(NORD_INT_ID_XPU_SEC, GIC_HIGHEST_SEC_PRIORITY,
+		       INTR_GROUP0,
+		       GIC_INTR_CFG_EDGE),
+};
+
+const unsigned int qti_interrupt_props_num = ARRAY_SIZE(qti_interrupt_props);
